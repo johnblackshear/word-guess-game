98736886ac41876524620d62f53083fe76af644b
John Blackshear <swjaybee@gmail.com>
2019-04-26 20:16:28 -0500

    init

---
+++ script.js
@@ -0,0 +1,21 @@
+chosenNumber = getRandomNumber()
+
+
+function getRandomNumber(){
+
+    
+    return Math.floor(Math.random()* 50);
+    //return rand;
+}
+
+function checkUserGuess(){
+
+}
+
+function dipslayGuess(){
+
+}
+
+function resestGame(){
+
+}
