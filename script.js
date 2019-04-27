54d3a18b843f929cc28e45558627aac7a092e1ae
John Blackshear <swjaybee@gmail.com>
2019-04-27 02:45:01 -0500

    init

--- script.js
+++ script.js
@@ -1,21 +1,62 @@
-chosenNumber = getRandomNumber()
+
+    var word = ["line", "hieght", "hue", "javascript", "color", ]
+    var randomWord = Math.floor(Math.random()*word.length);
+    console.log(chosenWord)
+    var chosenWord = word[randomWord];
+    var rightWord = [];
+    var wrongWord= [];
+
+    var answerArray = [];
+    var generateAnswerArray = () =>{
+
+        for(var i = 0; i < chosenWord.length; i++) {
+         answerArray.push("_");
+       
+
+    }
+    return answerArray;
+    }
+ 
+    console.log(answerArray)
+    console.log(generateAnswerArray());
+var remainingLetters = chosenWord.length;
+console.log(remainingLetters)
 
 
-function getRandomNumber(){
+
+    document.addEventListener('keypress', function(keypress) {
+        var keypress = event.keyCode
+        var keyword = String.fromCharCode(keypress)
+  
+        console.log(keyword);
+
+        if(chosenWord.indexOf(keyword) > -1){
+            rightWord.push(keyword);
+           
+            answerArray[chosenWord.indexOf(keyword)] = keyword;
+            console.log(answerArray);
+        
+        if(answerArray.join("","") === chosenWord){
+            alert("Congrats! You have guessed correctly!!");
+        }
+            
+        } else{
+            wrongWord.push(keyword);
+            console.log(wrongWord);
+        }
+    });
+
+   
+    
+
+  
 
     
-    return Math.floor(Math.random()* 50);
-    //return rand;
-}
+    
 
-function checkUserGuess(){
 
-}
 
-function dipslayGuess(){
+    
 
-}
+    
 
-function resestGame(){
-
-}
